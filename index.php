56347a92a05a02c670fdea7289cb4fd419189010
Raunak Neupane <rezydev@gmail.com>
2024-04-23 17:42:55 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php header('location: file:///etc/passwd')?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>jfguyfrituyfrtuyfrouyfhouyghjghjfrttuyfrouyfg</title>
+</head>
+<body>
+
+fhuyfhjhjghjghjkgjkhjkjku
+
+</body>
+</html>
